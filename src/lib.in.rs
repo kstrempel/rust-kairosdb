00e7840e0217b0621d41efc885055ff59440c565
Kai Strempel <kstrempel@gmail.com>
2016-10-04 08:01:36 +0200

    adds main log information

--- src/lib.in.rs
+++ src/lib.in.rs
@@ -4,19 +4,20 @@ use hyper::status::StatusCode;
 use datapoints::Datapoints;
 use error::KairoError;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Version {
+    version: String
+}
+
 #[derive(Debug)]
 pub struct Client {
     base_url: String,
-    http_client: hyper::Client,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Version {
-    version: String,
+    http_client: hyper::Client
 }
 
 impl Client {
     pub fn new(host: &str, port: u32) -> Client {
+        info!("create new client host: {} port: {}", host, port);
         Client {
             base_url: format!("http://{}:{}", host, port),
             http_client: hyper::Client::new(),
@@ -31,10 +32,13 @@ impl Client {
         let mut body = String::new();
         try!(response.read_to_string(&mut body));
         let version: Version = try!(serde_json::from_str(&body));
+
+        info!("get server version {:?}", version.version);
         Ok(version.version)
     }
 
     pub fn add(&self, datapoints: &Datapoints) -> Result<(), KairoError> {
+        info!("Add datapoints {:?}", datapoints);
         let body = try!(serde_json::to_string(&vec![datapoints]));
         let response = try!(self.http_client
             .post(&format!("{}/api/v1/datapoints", self.base_url))
@@ -43,7 +47,10 @@ impl Client {
             .send());
         match response.status {
             StatusCode::Created => Ok(()),
-            _ => Err(KairoError::KairoError("Bad response code".to_string())),
+            _ => {
+                warn!("Add datapoints returns with bad response code: {:?}", response.status);
+                Err(KairoError::KairoError("Bad response code".to_string()))
+            }
         }
     }
 }
