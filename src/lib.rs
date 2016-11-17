f30dc818e3775e57017e45f9c17735c4d614adcc
Kai Strempel <kstrempel@gmail.com>
2016-11-17 07:13:39 +0100

    comments the main entries

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+/*
+ * Copyright 2016 Kai Strempel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![crate_type = "lib"]
+#![crate_name = "kairosdb"]
+
+//!
+//! A `Client` for KairosBD REST API
+//!
+//! The Client itself is used as the central access point, from which
+//! numerous operations are defined implementing each of the specific
+//! KairosDB APIs.
+
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
